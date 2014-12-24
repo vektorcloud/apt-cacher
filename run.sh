1d000ead5de2583e8d1c657d249b491792756f55
Bradley Cicenas <bradley.cicenas@gmail.com>
2014-12-23 21:58:01 -0600

    add log tail, set bind address to eth0

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-/usr/sbin/dnsspoof -f /etc/dnsspoof.conf &
+CONTAINER_IP=$(getent hosts `hostname` | awk '{print $1}')
+sed -i "s/_CONTAINER_IP_/$CONTAINER_IP/g" /etc/dnsspoof.conf
+sed -i "s/_CONTAINER_IP_/$CONTAINER_IP/g" /etc/nginx/nginx.conf
+/usr/sbin/dnsspoof -i eth0 -f /etc/dnsspoof.conf &
 /usr/sbin/nginx -c /etc/nginx/nginx.conf 
+tail -f /var/log/nginx/*
