1118931901614022f7ea38be5048fcc51f645e8f
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-11-13 14:35:17 -0500

    add exec to run wrapper

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ CONTAINER_IP=$(getent hosts `hostname` | awk '{print $1}')
 RESOLVER=$(grep nameserver /etc/resolv.conf | awk '{print $2}' | head -1)
 sed -i "s/_RESOLVER_/$RESOLVER/g" /etc/nginx/nginx.conf
 
-/usr/sbin/nginx -c /etc/nginx/nginx.conf
+exec /usr/sbin/nginx -c /etc/nginx/nginx.conf
