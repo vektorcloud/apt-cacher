e43b74779bef2f47f3b1567d944af8947175fab6
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-11-13 14:34:59 -0500

    run nginx in foreground

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 CONTAINER_IP=$(getent hosts `hostname` | awk '{print $1}')
 RESOLVER=$(grep nameserver /etc/resolv.conf | awk '{print $2}' | head -1)
 sed -i "s/_RESOLVER_/$RESOLVER/g" /etc/nginx/nginx.conf
+
 /usr/sbin/nginx -c /etc/nginx/nginx.conf
-tail -f /var/log/nginx/*
