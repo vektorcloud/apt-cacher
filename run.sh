fa083329dc27015424a7e30ebb66f65679a5dd17
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-09-04 03:35:37 -0400

    parameterize cachedir

--- run.sh
+++ run.sh
@@ -24,5 +24,6 @@ function init_cachedirs() {
 CONTAINER_IP=$(getent hosts `hostname` | awk '{print $1}')
 RESOLVER=$(grep nameserver /etc/resolv.conf | awk '{print $2}' | head -1)
 sed -i "s/_RESOLVER_/$RESOLVER/g" /etc/nginx/nginx.conf
+sed -i "s/_CACHEDIR_/$CACHEDIR/g" /etc/nginx/nginx.conf
 
 exec /usr/sbin/nginx -c /etc/nginx/nginx.conf
